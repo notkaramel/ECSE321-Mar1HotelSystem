224d781667668eabf47feaee4dd1eec973c43b04
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2023-10-05 20:09:33 -0400

    added a bit of payment test

--- backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/tests/PaymentRepositoryTest.java
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/tests/PaymentRepositoryTest.java
@@ -1,16 +1,25 @@
 package ca.mcgill.ecse321.Mar1HotelSystem.tests;
 
- import static org.junit.jupiter.api.Assertions.assertEquals;
- import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
- import org.junit.jupiter.api.AfterEach;
- import org.junit.jupiter.api.Test;
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.boot.test.context.SpringBootTest;
+import ca.mcgill.ecse321.Mar1HotelSystem.dao.PaymentRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
- import ca.mcgill.ecse321.Mar1HotelSystem.model.Payment;
- import ca.mcgill.ecse321.Mar1HotelSystem.dao.PaymentRepository;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Payment;
+import ca.mcgill.ecse321.Mar1HotelSystem.dao.PaymentRepository;
 
-public class PaymentRepositoryTest {
+public class PaymentRepositoryTest {    //Setting up the customer repository
+ @Autowired
+ private PaymentRepository paymentRepository;
+
+ //Clearing the database after the test
+ @AfterEach
+ public void clearDatabase() {
+  paymentRepository.deleteAll();
+ }
 
 }
