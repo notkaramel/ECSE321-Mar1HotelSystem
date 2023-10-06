1881922c4ff2513c3dcf561a89877c9962baad4d
Lucaspac5 <lucas.pacicco@mail.mcgill.ca>
2023-10-06 17:27:50 -0400

    Update Account Repository Test
    Co-authored-by: Lucas Pacicco <Lucaspac5>
    Co-authored-by: Emma Friesen <emma.friesen@mail.mcgill.ca>

--- backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/tests/AccountRepositoryTest.java
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/tests/AccountRepositoryTest.java
@@ -1,14 +1,11 @@
 package ca.mcgill.ecse321.Mar1HotelSystem.tests;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import ca.mcgill.ecse321.Mar1HotelSystem.dao.AccountRepository;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Account;
 
