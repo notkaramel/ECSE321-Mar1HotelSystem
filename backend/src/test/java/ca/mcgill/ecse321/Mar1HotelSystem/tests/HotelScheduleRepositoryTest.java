1fb295e0073e544ca94759298712d4851a221d6f
bmokhtari <bilar.mokhtari@mail.mcgill.ca>
2023-10-06 15:29:25 -0400

    Hotel Schedule Works

---
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/tests/HotelScheduleRepositoryTest.java
@@ -0,0 +1,80 @@
+package ca.mcgill.ecse321.Mar1HotelSystem.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.Mar1HotelSystem.dao.CustomHoursRepository;
+import ca.mcgill.ecse321.Mar1HotelSystem.dao.HotelScheduleRepository;
+import ca.mcgill.ecse321.Mar1HotelSystem.dao.OperatingHoursRepository;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.CustomHours;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.HotelSchedule;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.OperatingHours;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.OperatingHours.DayOfWeek;
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+public class HotelScheduleRepositoryTest {
+    @Autowired
+    private HotelScheduleRepository hotelScheduleRepository;
+
+    @Autowired 
+    private CustomHoursRepository customHoursRepository;
+
+    @Autowired 
+    private OperatingHoursRepository operatingHoursRepository;
+
+
+
+    @BeforeEach
+    @AfterEach
+    public void clearDatabase(){
+        hotelScheduleRepository.deleteAll();
+        operatingHoursRepository.deleteAll();
+        customHoursRepository.deleteAll();
+    }
+
+    @Test
+    @Transactional
+    public void testPersistAndLoadHotelSchedule(){
+        Date date = new Date();
+        CustomHours customHours = new CustomHours(date, 8, 20);
+        OperatingHours operatingHours = new OperatingHours(DayOfWeek.Monday, 8, 20);
+        CustomHours[] customHoursArray = new CustomHours[1];
+        OperatingHours[] operatingHoursArray = new OperatingHours[1];
+        customHoursRepository.save(customHours);
+        customHours = customHoursRepository.findCustomHoursByDate(date);
+        operatingHoursRepository.save(operatingHours);
+        operatingHours = operatingHoursRepository.findOperatingHoursByOpeningHour(8);
+
+        customHoursArray[0] = customHours;
+        operatingHoursArray[0] = operatingHours;
+        HotelSchedule hotelSchedule = new HotelSchedule(2023, operatingHoursArray, customHoursArray);
+        hotelScheduleRepository.save(hotelSchedule);
+        hotelSchedule = hotelScheduleRepository.findHotelScheduleByYear(2023);
+
+        // Assertions
+        assertNotNull(hotelSchedule);
+        assertEquals(2023, hotelSchedule.getYear());
+
+        // Ensure the size of the retrieved customHours and operatingHours lists
+        assertEquals(1, hotelSchedule.getCustomHours().size());
+        assertEquals(1, hotelSchedule.getOperatingHours().size());
+
+        // Validate CustomHours details
+        CustomHours retrievedCustomHour1 = hotelSchedule.getCustomHours().get(0);
+        assertEquals(date, retrievedCustomHour1.getDate());
+
+        // Validate OperatingHours details
+        OperatingHours retrievedOperatingHour = hotelSchedule.getOperatingHours().get(0);
+        assertEquals(DayOfWeek.Monday, retrievedOperatingHour.getDayOfWeek());
+
+    }
+}
