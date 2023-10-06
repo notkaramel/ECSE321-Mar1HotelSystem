458cb81b1703f29de2bf7ef0fd917fecc69d2448
Antoine Phan <antoine.phan@mail.mcgill.ca>
2023-10-06 15:26:44 -0400

    Removed unused imports

--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/dao/ServiceRepository.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/dao/ServiceRepository.java
@@ -2,7 +2,6 @@ package ca.mcgill.ecse321.Mar1HotelSystem.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Service;
-import ca.mcgill.ecse321.Mar1HotelSystem.model.Request;
 
 public interface ServiceRepository extends CrudRepository<Service, Integer> {
     public Service findServiceByServiceId(int serviceId);
