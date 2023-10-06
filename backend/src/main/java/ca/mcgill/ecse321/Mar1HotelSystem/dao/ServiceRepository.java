2c43113c800d039610724b6861c104ba5c156a4b
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2023-10-06 15:23:22 -0400

    updated the service test

--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/dao/ServiceRepository.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/dao/ServiceRepository.java
@@ -5,6 +5,5 @@ import ca.mcgill.ecse321.Mar1HotelSystem.model.Service;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Request;
 
 public interface ServiceRepository extends CrudRepository<Service, Integer> {
-    public Service findServiceByRequest(Request request);
     public Service findServiceByServiceId(int serviceId);
 }
