0fbef5cfe2543b7e88ee8d2933b47e2842fd6cf8
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2023-10-05 20:29:01 -0400

    manually corrected the formatting

--- backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/tests/PaymentRepositoryTest.java
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/tests/PaymentRepositoryTest.java
@@ -13,32 +13,32 @@ import ca.mcgill.ecse321.Mar1HotelSystem.model.Payment;
 import ca.mcgill.ecse321.Mar1HotelSystem.dao.PaymentRepository;
 
 public class PaymentRepositoryTest {
- // Setting up the payment repository
- @Autowired
- private PaymentRepository paymentRepository;
+    // Setting up the payment repository
+    @Autowired
+    private PaymentRepository paymentRepository;
 
- // Clearing the database after the test
- @AfterEach
- public void clearDatabase() {
-  paymentRepository.deleteAll();
- }
+    // Clearing the database after the test
+    @AfterEach
+    public void clearDatabase() {
+        paymentRepository.deleteAll();
+    }
 
- @Test
- public void testPersistAndReadPayment () {
-  // Creating the payment
-  int amount = 5;
-  int id = 1;
-  Payment payment = new Payment(amount, id);
+    @Test
+    public void testPersistAndReadPayment () {
+        // Creating the payment
+        int amount = 5;
+        int id = 1;
+        Payment payment = new Payment(amount, id);
 
-  // Adding the payment to the persistence layer
-  paymentRepository.save(payment);
+        // Adding the payment to the persistence layer
+        paymentRepository.save(payment);
 
-  // Read from the database
-  payment = paymentRepository.findPaymentByPaymentId(id);
+        // Read from the database
+        payment = paymentRepository.findPaymentByPaymentId(id);
 
-  // Asserting the infog
-  assertNotNull(payment);
-  assertEquals(amount, payment.getAmount());
-  assertEquals(id, payment.getPaymentId());
- }
+        // Asserting the infog
+        assertNotNull(payment);
+        assertEquals(amount, payment.getAmount());
+        assertEquals(id, payment.getPaymentId());
+    }
 }
