46c4732de779abe5a70bb5776df0dad467ca2fe4
Antoine Phan <antoine.phan@mail.mcgill.ca>
2023-10-06 17:32:48 -0400

    Rewrite findHotelBySchedule

--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/dao/HotelRepository.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/dao/HotelRepository.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse321.Mar1HotelSystem.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Hotel;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.HotelSchedule;
 
 /**
  * The CRUD Repository Interface to store and retrieve all Hotel objects.
@@ -16,4 +17,12 @@ public interface HotelRepository extends CrudRepository<Hotel, String> {
      * @return the corresponding Hotel object
      */
     public Hotel findHotelByHotelName(String hotelName);
+
+    /**
+     * Find a Hotel object by its hotelSchedule.
+     * 
+     * @param hotelSchedule
+     * @return the corresponding Hotel object
+     */
+    public Hotel findHotelByHotelSchedule(HotelSchedule hotelSchedule);
 }
