d5f3365f158f9ba2ef24f5150a27a6f4c4687648
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2023-10-21 18:23:36 -0400

    added boiler imports for the tests

---
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/CustomerServiceTest.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse321.Mar1HotelSystem.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+public class CustomerServiceTest {
+}
