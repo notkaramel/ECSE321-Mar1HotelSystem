1d6ef095e43d56211bf790f42616537b92411e3a
Ad2Am2 <92311639+Ad2Am2@users.noreply.github.com>
2023-10-28 14:39:23 -0400

    Initial commit and boilerplate code

---
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/RequestServiceTest.java
@@ -0,0 +1,26 @@
+package ca.mcgill.ecse321.Mar1HotelSystem.service;
+
+import ca.mcgill.ecse321.Mar1HotelSystem.dao.RequestRepository;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Request;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+public class RequestServiceTest {
+    
+}
