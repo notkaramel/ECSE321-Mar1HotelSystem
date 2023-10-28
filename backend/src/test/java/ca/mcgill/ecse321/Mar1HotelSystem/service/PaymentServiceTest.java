cc5edcbd32e39685fa1f721b1e1affdf6c8bf59f
bmokhtari <bilar.mokhtari@mail.mcgill.ca>
2023-10-28 19:57:47 -0400

    Payment Service Tests Completed

--- backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/PaymentServiceTest.java
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/PaymentServiceTest.java
@@ -1,26 +1,72 @@
 package ca.mcgill.ecse321.Mar1HotelSystem.service;
 
-import ca.mcgill.ecse321.Mar1HotelSystem.dao.PaymentRepository;
-import ca.mcgill.ecse321.Mar1HotelSystem.model.Payment;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Payment;
+import ca.mcgill.ecse321.Mar1HotelSystem.dao.PaymentRepository;
+
+@SpringBootTest
 public class PaymentServiceTest {
-    
+
+    @Mock
+    private PaymentRepository paymentRepository;
+
+    @InjectMocks
+    private PaymentService paymentService;
+
+    @Test
+    public void testCreatePayment() {
+        Payment payment = new Payment(100);
+        when(paymentRepository.save(any(Payment.class))).thenReturn(payment);
+
+        Payment createdPayment = paymentService.createPayment(100);
+
+        assertNotNull(createdPayment);
+        assertEquals(100, createdPayment.getAmount());
+    }
+
+    @Test
+    public void testGetPaymentById() {
+        Payment payment = new Payment(100);
+        when(paymentRepository.findPaymentByPaymentId(1)).thenReturn(payment);
+
+        Payment foundPayment = paymentService.getPaymentById(1);
+
+        assertNotNull(foundPayment);
+        assertEquals(100, foundPayment.getAmount());
+    }
+
+@Test
+public void testDeletePaymentById() {
+    Payment payment = new Payment(100);
+    payment.setPaymentId(1); // Assuming that your Payment object has a setPaymentId method
+    when(paymentRepository.findPaymentByPaymentId(anyInt())).thenReturn(payment);
+    doNothing().when(paymentRepository).delete(any(Payment.class));
+    boolean isDeleted = paymentService.deletePaymentById(1);
+    assertTrue(isDeleted);
+    verify(paymentRepository, times(1)).delete(any(Payment.class));
+}
+
+
+    @Test
+    public void testGetAllPayments() {
+        Payment payment1 = new Payment(100);
+        Payment payment2 = new Payment(200);
+        when(paymentRepository.findAll()).thenReturn(List.of(payment1, payment2));
+
+        List<Payment> payments = paymentService.getAllPayments();
+
+        assertEquals(2, payments.size());
+    }
+
 }
