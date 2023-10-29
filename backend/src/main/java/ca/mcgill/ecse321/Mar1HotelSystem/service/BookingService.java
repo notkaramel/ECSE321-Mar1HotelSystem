9895466821eb294e7c53ba900f36a2033a5d4bf3
bmokhtari <bilar.mokhtari@mail.mcgill.ca>
2023-10-28 22:45:31 -0400

    Booking Service Test v1

---
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/service/BookingService.java
@@ -0,0 +1,77 @@
+package ca.mcgill.ecse321.Mar1HotelSystem.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.Mar1HotelSystem.dao.BookingRepository;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Booking;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.GeneralUser;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Payment;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Room;
+
+/**
+ * Service class to manage Booking Entities
+ * 
+ * @author Bilar Mokhtari
+ */
+@Service
+public class BookingService {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    /**
+     * Service method to create and save a new booking.
+     */
+    @Transactional
+    public Booking createBooking(Payment payment, GeneralUser generalUser, Room room) {
+        Booking booking = new Booking();
+        booking.setPayment(payment);
+        booking.setGeneralUser(generalUser);
+        booking.setRoom(room);
+        bookingRepository.save(booking);
+        return booking;
+    }
+
+    /**
+     * Service method to update an existing booking.
+     */
+    @Transactional
+    public Booking updateBooking(Booking booking) {
+        return bookingRepository.save(booking);
+    }
+
+    /**
+     * Service method to delete a booking. 
+     */
+    @Transactional
+    public boolean deleteBooking(int bookingId) {
+        Booking booking = bookingRepository.findBookingByBookingId(bookingId);
+        try {
+            bookingRepository.delete(booking);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    };
+    
+
+    /**
+     * Service method to retrieve a booking by ID.
+     */
+    @Transactional
+    public Booking getBookingById(int bookingId) {
+        return bookingRepository.findBookingByBookingId(bookingId);
+    }
+
+    /**
+     * Service method to retrieve all bookings.
+     */
+    @Transactional
+    public Iterable<Booking> getAllBookings() {
+        return bookingRepository.findAll();
+    }
+}
--- backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/BookingServiceTest.java
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/BookingServiceTest.java
@@ -2,9 +2,15 @@ package ca.mcgill.ecse321.Mar1HotelSystem.service;
 
 import ca.mcgill.ecse321.Mar1HotelSystem.dao.BookingRepository;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Booking;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Customer;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.GeneralUser;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Payment;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Room;
 
+import org.checkerframework.checker.units.qual.t;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -12,15 +18,116 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.sql.Date;
+import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
+/*
+ * @Author: Bilar Mokhtari
+ */
+@SpringBootTest
 public class BookingServiceTest {
+    @Mock
+    private BookingRepository bookingRepository;
+
+    @InjectMocks
+    private BookingService bookingService;
+
+    /*
+     * Test to make sure if the booking is created correctly.
+     */
+    @Test
+    public void testCreateBooking() {
+        String generalUserEmail = "john.doe@example.com";
+        GeneralUser generalUser = new GeneralUser();
+        generalUser.setEmail(generalUserEmail);
+        int roomId = 1;
+        Payment payment = new Payment();
+        payment.setAmount(100);
+        Room room = new Room();
+        room.setRoomId(roomId);
+        Booking booking = new Booking();
+        booking.setGeneralUser(generalUser);
+        booking.setRoom(room);
+        booking.setPayment(payment);
+        when(bookingRepository.save(booking)).thenReturn(booking);
+        Booking createdBooking = bookingService.createBooking(booking.getPayment(), booking.getGeneralUser(), booking.getRoom());
+
+        assertNotNull(createdBooking);
+        assertEquals(generalUserEmail, createdBooking.getGeneralUser().getEmail());
+        assertEquals(roomId, createdBooking.getRoom().getRoomId());
+        assertEquals(booking.getBookingId(), createdBooking.getBookingId());
+        assertEquals(100, createdBooking.getPayment().getAmount());
+        
+
+
     
+    }
+    /*
+     * Test to see if the booking is updated correctly.
+     */
+    @Test
+    public void testUpdateBooking() {
+        Booking booking = new Booking();
+        booking.setBookingId(1);
+        when(bookingRepository.save(booking)).thenReturn(booking);
+        Booking updatedBooking = bookingService.updateBooking(booking);
+        assertNotNull(updatedBooking);
+        assertEquals(1, updatedBooking.getBookingId());
+    }
+
+    /*
+     * Test to see if the booking is deleted correctly.
+     */
+    @Test
+    public void testDeleteBooking() {
+        Booking booking = new Booking();
+        booking.setBookingId(1);
+        when(bookingRepository.findBookingByBookingId(booking.getBookingId())).thenReturn(booking);
+        doNothing().when(bookingRepository).delete(booking);
+        boolean isDeleted = bookingService.deleteBooking(1);
+        assertTrue(isDeleted);
+    }
+    /*
+     * Test to see if you can get a booking by its ID.
+     */
+    @Test
+    public void testGetBookingById() {
+        Booking booking = new Booking();
+        booking.setBookingId(1);
+        when(bookingRepository.findBookingByBookingId(1)).thenReturn(booking);
+        Booking foundBooking = bookingService.getBookingById(1);
+        assertNotNull(foundBooking);
+        assertEquals(1, foundBooking.getBookingId());
+    }
+    /*
+     * Test to see if bookings are stored correctly and you can get them all.
+     */
+    @Test
+    public void testGetAllBookings() {
+        Booking booking1 = new Booking();
+        Booking booking2 = new Booking();
+        when(bookingRepository.findAll()).thenReturn(List.of(booking1, booking2));
+        List<Booking> bookings = ServiceUtils.toList(bookingService.getAllBookings());
+        assertEquals(2, bookings.size());
+    }
+    /*
+     * Test to make sure there are no bookings when none have been made.
+     */
+    @Test
+    public void testGetAllBookingsEmpty() {
+        when(bookingRepository.findAll()).thenReturn(List.of());
+        List<Booking> bookings = ServiceUtils.toList(bookingService.getAllBookings());
+        assertEquals(0, bookings.size());
+    }
+
+
+
+
 }
