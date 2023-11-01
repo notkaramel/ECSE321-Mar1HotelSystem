d1cb4340e5b0d55efe54c2892180cf25a77b536c
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2023-11-01 15:11:23 -0400

    added email checking for customer and employee

--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/service/EmployeeService.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/service/EmployeeService.java
@@ -1,6 +1,8 @@
 package ca.mcgill.ecse321.Mar1HotelSystem.service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import jakarta.transaction.Transactional;
  * 
  * @author Antoine Phan (@notkaramel)
  * @author Lucas Pacicco (@Lucaspac5)
+ * @author ZiXu Liu (@ARandomPi)
  * 
  */
 
@@ -28,13 +31,37 @@ public class EmployeeService {
 
     @Transactional
     public Employee getEmployee(String email) {
-        Employee employee = employeeRepository.findEmployeeByEmail(email);
-        return employee;
+        String emailTrimmed = email.trim();
+        return employeeRepository.findEmployeeByEmail(email);
     }
 
     @Transactional
     public Employee createEmployee(String firstName, String lastName, String email, int phoneNumber, String password,
             int hoursWorked) {
+        // Check if firstName is empty
+        if (firstName == null || firstName.trim().isEmpty()) {
+            throw new IllegalArgumentException("The first name cannot be empty!");
+        }
+        // Check if lastName is empty
+        if (lastName == null || lastName.trim().isEmpty()) {
+            throw new IllegalArgumentException("The last name cannot be empty!");
+        }
+        // Check if email is empty
+        if (email == null || email.trim().isEmpty()) {
+            throw new IllegalArgumentException("The email cannot be empty!");
+        }
+        // Check if email is valid
+        String emailTrimmed = email.trim();
+        Pattern pattern = Pattern.compile("^(\\S+)@(\\S+)\\.((com)|(ca))$");
+        Matcher matcher = pattern.matcher(emailTrimmed);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("The email is invalid!");
+        }
+        // Check if password is empty
+        if (password == null || password.trim().isEmpty()) {
+            throw new IllegalArgumentException("The password cannot be empty!");
+        }
+        // Create, save, and return the employee
         Employee employee = new Employee(firstName, lastName, email, phoneNumber, password, hoursWorked);
         employeeRepository.save(employee);
         return employee;
