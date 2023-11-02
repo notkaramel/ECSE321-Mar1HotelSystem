b5486c67d10fe63eea6ca7e22f654532db775687
b.mokhtari <bilar.mokhtari@mail.mcgill.ca>
2023-11-02 11:53:10 -0400

    It COMPILES

--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/exception/Mar1HotelSystemExceptionHandler.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/exception/Mar1HotelSystemExceptionHandler.java
@@ -4,13 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
 @ControllerAdvice
 public class Mar1HotelSystemExceptionHandler {
-    public Mar1HotelSystemExceptionHandler() {}
-        
-    @ExceptionHandler(Mar1HotelSystemException.class)
-    public ResponseEntity<String> handleMar1HotelSystemException(Mar1HotelSystemException e) {
-        return new ResponseEntity<String>(e.getMessage(), e.getStatus());
-    }
+        @ExceptionHandler(Mar1HotelSystemException.class)
+        public ResponseEntity<String> handleMar1HotelSystemException(Mar1HotelSystemException e) {
+            return new ResponseEntity<String>(e.getMessage(), e.getStatus());
+
+        }
 }
