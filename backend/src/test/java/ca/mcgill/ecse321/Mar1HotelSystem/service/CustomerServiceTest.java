6ee1cfb937709fd7c4c43ee53353c6f9f2954a86
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2023-11-02 23:16:13 -0400

    added new tests, still one test failing

--- backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/CustomerServiceTest.java
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/CustomerServiceTest.java
@@ -3,12 +3,15 @@ package ca.mcgill.ecse321.Mar1HotelSystem.service;
 import ca.mcgill.ecse321.Mar1HotelSystem.dao.CustomerRepository;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Customer;
 
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Employee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
@@ -34,8 +37,11 @@ public class CustomerServiceTest {
     @InjectMocks
     private CustomerService customerService;
 
-    private static final String CUSTOMER_KEY = "TestEmail";
-    private static final String NONEXISTING_KEY = "NotACustomer";
+    private static final String CUSTOMER_KEY = "TestEmail@gmail.com";
+    private static final String NONEXISTING_KEY = "NotACustomer@mail.com";
+
+    private static final String CUSTOMER_INITIAL_KEY_1 = "test1@mail.com";
+    private static final String CUSTOMER_INITIAL_KEY_2 = "test2@mail.com";
 
     /**
      * Set mock customer output before each test.
@@ -43,11 +49,11 @@ public class CustomerServiceTest {
     @BeforeEach
     public void setMockOutput() {
         lenient().when(customerDao.findCustomerByEmail(anyString())).thenAnswer((invocation) -> {
-            if (invocation.getArgument(0).equals(CUSTOMER_KEY)) {
+            if (invocation.getArgument(0).equals("test1@mail.com")) {
                 return new Customer(
                         "Josh",
                         "Deb",
-                        CUSTOMER_KEY,
+                        CUSTOMER_INITIAL_KEY_1,
                         1234567890,
                         "TestPassword"
 
@@ -58,16 +64,29 @@ public class CustomerServiceTest {
         });
         lenient().when(customerDao.findAll()).thenAnswer((invocation) -> {
             ArrayList<Customer> customers = new ArrayList<>();
-            Customer customer = new Customer(
+            Customer customer1 = new Customer(
                     "Josh",
                     "Deb",
-                    CUSTOMER_KEY,
+                    CUSTOMER_INITIAL_KEY_1,
                     1234567890,
                     "TestPassword"
             );
-            customers.add(customer);
+            customers.add(customer1);
+            Customer customer2 = new Customer(
+                    "Seth",
+                    "Royal",
+                    CUSTOMER_INITIAL_KEY_2,
+                    1231232312,
+                    "TestPassword2"
+            );
+            customers.add(customer2);
             return customers;
         });
+        Answer<?> returnParameterAsAnswer =
+                (InvocationOnMock invocation) -> {
+                    return invocation.getArgument(0);
+                };
+        lenient().when(customerDao.save(any(Customer.class))).thenAnswer(returnParameterAsAnswer);
     }
 
     /**
@@ -294,11 +313,35 @@ public class CustomerServiceTest {
     }
 
     /**
+     * Test creating a customer with an existing email
+     */
+    @Test
+    public void testCreateCustomerExistingEmail() {;
+        String error = null;
+        Customer customer = null;
+        try {
+            customer = customerService.createCustomer(
+                    "Josh",
+                    "Deb",
+                    CUSTOMER_INITIAL_KEY_1,
+                    1234567890,
+                    "TestPassword"
+            );
+        } catch (IllegalArgumentException e) {
+            error = e.getMessage();
+        }
+        // check null
+        assertNull(customer);
+        // check error
+        assertEquals("User using that email already exists!", error);
+    }
+
+    /**
      * Test retrieving a customer.
      */
     @Test
     public void testGetExistingGeneralUser() {
-        assertEquals(CUSTOMER_KEY, customerService.getCustomer(CUSTOMER_KEY).getEmail());
+        assertEquals(CUSTOMER_INITIAL_KEY_1, customerService.getCustomer(CUSTOMER_INITIAL_KEY_1).getEmail());
     }
 
     /**
@@ -307,5 +350,163 @@ public class CustomerServiceTest {
     @Test
     public void testGetNonExistingPerson() {
         assertNull(customerService.getCustomer(NONEXISTING_KEY));
+    }
+
+    /**
+     * Test retrieving all customers.
+     */
+    @Test
+    public void testGetAllCustomers() {
+        ArrayList<Customer> listOfCustomers = (ArrayList<Customer>) customerService.getAllCustomers();
+        assertEquals(2, listOfCustomers.size());
+        for(Customer customer : listOfCustomers) {
+            assertNotNull(customer);
+        }
+    }
+
+    /**
+     * Test updating a customer.
+     */
+    @Test
+    public void testUpdateCustomer() {
+        String newFirstName = "Josh";
+        String newLastName = "Deb";
+        String newPassword = "TestPassword";
+        long newPhoneNumber = 1234567890;
+        Customer customer = null;
+        try {
+            customer = customerService.updateCustomer(
+                    newFirstName,
+                    newLastName,
+                    newPhoneNumber,
+                    newPassword,
+                    CUSTOMER_INITIAL_KEY_1);
+        } catch (IllegalArgumentException e) {
+            // Check that no error occurred
+            fail();
+        }
+        assertNotNull(customer);
+        assertEquals(CUSTOMER_INITIAL_KEY_1, customer.getEmail());
+    }
+
+    /**
+     * Test updating a customer with empty first name.
+     */
+    @Test
+    public void testUpdateCustomerInvalidFirstName() {
+        String newFirstName = "";
+        String newLastName = "Deb";
+        String newPassword = "TestPassword";
+        long newPhoneNumber = 1234567890;
+        Customer customer = null;
+        String error = null;
+        try {
+            customer = customerService.updateCustomer(
+                    newFirstName,
+                    newLastName,
+                    newPhoneNumber,
+                    newPassword,
+                    CUSTOMER_INITIAL_KEY_1);
+        } catch (IllegalArgumentException e) {
+            error = e.getMessage();
+        }
+        assertNull(customer);
+        assertEquals("The first name cannot be empty!", error);
+    }
+
+    /**
+     * Test updating a customer with empty last name.
+     */
+    @Test
+    public void testUpdateCustomerInvalidLastName() {
+        String newFirstName = "Josh";
+        String newLastName = "";
+        String newPassword = "TestPassword";
+        long newPhoneNumber = 1234567890;
+        Customer customer = null;
+        String error = null;
+        try {
+            customer = customerService.updateCustomer(
+                    newFirstName,
+                    newLastName,
+                    newPhoneNumber,
+                    newPassword,
+                    CUSTOMER_INITIAL_KEY_1);
+        } catch (IllegalArgumentException e) {
+            error = e.getMessage();
+        }
+        assertNull(customer);
+        assertEquals("The last name cannot be empty!", error);
+    }
+
+    /**
+     * Test updating a customer with empty password.
+     */
+    @Test
+    public void testUpdateCustomerInvalidPassword() {
+        String newFirstName = "Josh";
+        String newLastName = "Deb";
+        String newPassword = "";
+        long newPhoneNumber = 1234567890;
+        Customer customer = null;
+        String error = null;
+        try {
+            customer = customerService.updateCustomer(
+                    newFirstName,
+                    newLastName,
+                    newPhoneNumber,
+                    newPassword,
+                    CUSTOMER_INITIAL_KEY_1);
+        } catch (IllegalArgumentException e) {
+            error = e.getMessage();
+        }
+        assertNull(customer);
+        assertEquals("The password cannot be empty!", error);
+    }
+
+    @Test
+    public void testUpdateCustomerEmailNotFound() {
+        String newFirstName = "Josh";
+        String newLastName = "Deb";
+        String newPassword = "TestPassword";
+        long newPhoneNumber = 1234567890;
+        Customer customer = null;
+        String error = null;
+        try {
+            customer = customerService.updateCustomer(
+                    newFirstName,
+                    newLastName,
+                    newPhoneNumber,
+                    newPassword,
+                    NONEXISTING_KEY);
+        } catch (IllegalArgumentException e) {
+            error = e.getMessage();
+        }
+        assertNull(customer);
+        assertEquals("The customer does not exist!", error);
+    }
+
+    /**
+     * Test deleting a customer.
+     */
+    @Test
+    public void deleteCustomer() {
+        boolean deleted = false;
+        Customer customer = null;
+        try {
+            customer = customerService.createCustomer(
+                    "Josh",
+                    "Deb",
+                    CUSTOMER_KEY,
+                    1234567890,
+                    "TestPassword"
+            );
+            deleted = customerService.deleteCustomer(CUSTOMER_KEY);
+        } catch (Exception e) {
+            fail();
+        }
+        assertTrue(deleted);
+        assertNull(customer);
+        assertNull(customerService.getCustomer(CUSTOMER_KEY));
     }
 }
--- backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/EmployeeServiceTest.java
+++ backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/EmployeeServiceTest.java
@@ -1,6 +1,7 @@
 package ca.mcgill.ecse321.Mar1HotelSystem.service;
 
 import ca.mcgill.ecse321.Mar1HotelSystem.dao.EmployeeRepository;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Customer;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Employee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,7 +37,10 @@ public class EmployeeServiceTest {
     private EmployeeService employeeService;
 
     private static final String EMPLOYEE_KEY = "TestEmployee@mail.mcgill.ca";
-    private static final String NONEXISTING_KEY = "NotAnEmployee";
+    private static final String NONEXISTING_KEY = "NotAnEmployee@mail.com";
+
+    private static final String EMPLOYEE_INITIAL_KEY_1 = "testemployee1@mail.com";
+    private static final String EMPLOYEE_INITIAL_KEY_2 = "testemployee2@mail.com";
 
     /**
      * Set mock employee output before each test.
@@ -44,11 +48,11 @@ public class EmployeeServiceTest {
     @BeforeEach
     public void setMockOutput() {
         lenient().when(employeeDao.findEmployeeByEmail(anyString())).thenAnswer((invocation) -> {
-            if (invocation.getArgument(0).equals(EMPLOYEE_KEY)) {
+            if (invocation.getArgument(0).equals(EMPLOYEE_INITIAL_KEY_1)) {
                 return new Employee(
                         "TestFirstName",
                         "TestLastName",
-                        EMPLOYEE_KEY,
+                        EMPLOYEE_INITIAL_KEY_1,
                         1234567890,
                         "TestPassword",
                         0
@@ -59,15 +63,24 @@ public class EmployeeServiceTest {
         });
         lenient().when(employeeDao.findAll()).thenAnswer((invocation) -> {
             ArrayList<Employee> employees = new ArrayList<>();
-            Employee employee = new Employee(
+            Employee employee1 = new Employee(
                     "TestFirstName",
                     "TestLastName",
-                    EMPLOYEE_KEY,
+                    EMPLOYEE_INITIAL_KEY_1,
                     1234567890,
                     "TestPassword",
                     0
             );
-            employees.add(employee);
+            employees.add(employee1);
+            Employee employee2 = new Employee(
+                    "TestFirstName2",
+                    "TestLastName2",
+                    EMPLOYEE_INITIAL_KEY_2,
+                    1234567890,
+                    "TestPassword2",
+                    0
+            );
+            employees.add(employee2);
             return employees;
         });
         Answer<?> returnParameterAsAnswer =
@@ -310,6 +323,31 @@ public class EmployeeServiceTest {
     }
 
     /**
+     * Test creating an employee with an existing email
+     */
+    @Test
+    public void testCreateEmployeeExistingEmail() {
+        String error = null;
+        Employee employee = null;
+        try {
+            employee = employeeService.createEmployee(
+                    "boi",
+                    "boi",
+                    EMPLOYEE_INITIAL_KEY_1,
+                    1234567891,
+                    " ",
+                    0
+            );
+        } catch (IllegalArgumentException e) {
+            error = e.getMessage();
+        }
+        // check null
+        assertNull(employee);
+        // check error
+        assertEquals("User using that email already exists!", error);
+    }
+
+    /**
      * Test retrieving an employee.
      */
     @Test
@@ -325,17 +363,113 @@ public class EmployeeServiceTest {
         assertNull(employeeService.getEmployee(NONEXISTING_KEY));
     }
 
+    /**
+     * Test retrieving all employees.
+     */
     @Test
-    public void testUpdateEmployeeFirstName() {
+    public void testGetAllCustomers() {
+        ArrayList<Employee> listOfEmployees = (ArrayList<Employee>) employeeService.getAllEmployees();
+        assertEquals(2, listOfEmployees.size());
+        for(Employee employee : listOfEmployees) {
+            assertNotNull(employee);
+        }
+    }
+
+    @Test
+    public void testUpdateEmployee() {
     	String newFirstName = "Dolan";
+        String newLastName = "Duck";
+        String newPassword = "password123";
     	Employee employee = null;
     	try {
-    		employee = employeeService.updateEmployeeFirstName(newFirstName, EMPLOYEE_KEY);
+    		employee = employeeService.updateEmployee(newFirstName, newLastName, newPassword, EMPLOYEE_INITIAL_KEY_1);
     	} catch (IllegalArgumentException e) {
     		fail();
     	}
     	// Check not null
         assertNotNull(employee);
     	assertEquals(newFirstName, employee.getFirstName());
+        assertEquals(newLastName, employee.getLastName());
+        assertEquals(newPassword, employee.getPassword());
+    }
+
+    @Test
+    public void testUpdateEmployeeInvalidFirstName() {
+        	String newFirstName = "";
+            String newLastName = "Duck";
+            String newPassword = "password123";
+        	Employee employee = null;
+        	String error = null;
+        	try {
+        		employee = employeeService.updateEmployee(newFirstName, newLastName, newPassword, EMPLOYEE_INITIAL_KEY_1);
+        	} catch (IllegalArgumentException e) {
+        		error = e.getMessage();
+        	}
+        	// Check null
+            assertNull(employee);
+        	assertEquals("The first name cannot be empty!", error);
+    }
+
+    @Test
+    void testUpdateEmployeeInvalidLastName() {
+        	String newFirstName = "Dolan";
+            String newLastName = "";
+            String newPassword = "password123";
+        	Employee employee = null;
+        	String error = null;
+        	try {
+        		employee = employeeService.updateEmployee(newFirstName, newLastName, newPassword, EMPLOYEE_INITIAL_KEY_1);
+        	} catch (IllegalArgumentException e) {
+        		error = e.getMessage();
+        	}
+        	// Check null
+            assertNull(employee);
+        	assertEquals("The last name cannot be empty!", error);
+    }
+
+    @Test
+    void testUpdateEmployeeInvalidPassword() {
+        String newFirstName = "Dolan";
+        String newLastName = "Duck";
+        String newPassword = "";
+        Employee employee = null;
+        String error = null;
+        try {
+            employee = employeeService.updateEmployee(newFirstName, newLastName, newPassword, EMPLOYEE_INITIAL_KEY_1);
+        } catch (IllegalArgumentException e) {
+            error = e.getMessage();
+        }
+        // Check null
+        assertNull(employee);
+        assertEquals("The password cannot be empty!", error);
+    }
+
+    @Test
+    public void testUpdateEmployeeEmailNotFound() {
+        	String newFirstName = "Dolan";
+            String newLastName = "Duck";
+            String newPassword = "password123";
+        	Employee employee = null;
+        	String error = null;
+        	try {
+        		employee = employeeService.updateEmployee(newFirstName, newLastName, newPassword, NONEXISTING_KEY);
+        	} catch (IllegalArgumentException e) {
+        		error = e.getMessage();
+        	}
+        	// Check null
+            assertNull(employee);
+        	assertEquals("The employee does not exist!", error);
+    }
+    @Test
+    public void testDeleteEmployee() {
+        boolean deleted = false;
+        try {
+            deleted = employeeService.deleteEmployee(EMPLOYEE_INITIAL_KEY_1);
+        } catch (Exception e) {
+            fail();
+        }
+        // Check not null
+        assertTrue(deleted);
+        assertNull(employeeService.getEmployee(EMPLOYEE_INITIAL_KEY_1));
     }
 }
