3ed415778451377cbb1c9ddee68c24cbb9daaa13
Lucaspac5 <lucas.pacicco@mail.mcgill.ca>
2023-11-04 21:22:49 -0400

    Initial GeneralUSer and Manager Rest Controllers

--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/controller/GeneralUserRestController.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/controller/GeneralUserRestController.java
@@ -1,8 +1,15 @@
 package ca.mcgill.ecse321.Mar1HotelSystem.controller;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse321.Mar1HotelSystem.model.GeneralUser;
 import ca.mcgill.ecse321.Mar1HotelSystem.service.GeneralUserService;
 /**
  * The controller that handles /guest endpoint requests
@@ -21,5 +28,37 @@ public class GeneralUserRestController {
     
     @Autowired
 	private GeneralUserService service;
+
+     @GetMapping(value = { "/generalUsers", "/generalUsers/" })
+    public List<GeneralUser> getAllGeneralUsers() {
+        List<GeneralUser> generalUsers = new ArrayList<>();
+        generalUsers = service.getAllGeneralUsers();
+        return generalUsers;
+    }
+
+    @GetMapping(value = { "/generalUsers/{email}", "/generalUsers/{email}/" })
+    public GeneralUser getGeneralUser(@PathVariable("email") String email) {
+        GeneralUser generalUser = service.getGeneralUser(email);
+        return generalUser;
+    }
+
+    @PostMapping(value = { "/generalUsers/{email}/{firstName}/{lastName}/{phoneNumber}", "/generalUsers/{email}/{firstName}/{lastName}/{phoneNumber}/" })
+    public GeneralUser createGeneralUser( @PathVariable("email") String email, @PathVariable("firstName") String firstName, 
+    @PathVariable("lastName") String lastName, @PathVariable("phoneNumber") int phoneNumber){
+        GeneralUser generalUser = service.createGeneralUser(firstName,lastName, email, phoneNumber);
+        return generalUser;
+    }
+
+    @PostMapping(value = { "/generalUsers/{oldEmail}/{newEmail}", "/generalUsers/{oldEmail}/{newEmail}/" })
+    public GeneralUser updateGeneralUser( @PathVariable("oldEmail") String oldEmail, @PathVariable("newEmail") String newEmail){
+        GeneralUser generalUser = service.updateGeneralUserEmail(oldEmail, newEmail);
+        return generalUser;
+    }
+
+    @PostMapping(value = { "/generalUsers/{email}", "/generalUsers/{email}/" })
+    public Boolean updateGeneralUser(@PathVariable("email") String email){
+        Boolean generalUser = service.deleteGeneralUser(email);
+        return generalUser;
+    }
 
 }
--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/controller/ManagerRestController.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/controller/ManagerRestController.java
@@ -1,9 +1,17 @@
 package ca.mcgill.ecse321.Mar1HotelSystem.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import ca.mcgill.ecse321.Mar1HotelSystem.dto.ManagerDto;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Manager;
 import ca.mcgill.ecse321.Mar1HotelSystem.service.ManagerService;
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * The controller that handles /manager endpoint requests
  * Required functionalities:
@@ -19,4 +27,37 @@ public class ManagerRestController {
     @Autowired
 	private ManagerService service;
     
+    @GetMapping(value = { "/managers", "/managers/" })
+    public List<Manager> getAllManagers() {
+        List<Manager> managers = new ArrayList<>();
+        managers = service.getAllManagers();
+        return managers;
+    }
+
+    @GetMapping(value = { "/managers/{email}", "/managers/{email}/" })
+    public Manager getManager(@PathVariable("email") String email) {
+        Manager manager = service.getManager(email);
+        return manager;
+    }
+
+    @PostMapping(value = { "/managers/{email}/{firstName}/{lastName}/{phoneNumber}/{password}", "/managers/{email}/{firstName}/{lastName}/{phoneNumber}/{password}/" })
+    public Manager createManager( @PathVariable("email") String email, @PathVariable("firstName") String firstName, 
+    @PathVariable("lastName") String lastName, @PathVariable("phoneNumber") int phoneNumber, 
+    @PathVariable("password") String password){
+        Manager manager = service.createManager(firstName,lastName, email, phoneNumber, password);
+        return manager;
+    }
+
+    @PostMapping(value = { "/managers/{email}/{oldPassword}/{newPassword}", "/managers/{email}/{oldPassword}/{newPassword}/" })
+    public Manager updateManager( @PathVariable("email") String email, @PathVariable("oldPassword") String oldPassword, 
+    @PathVariable("newPassword") String newPassword){
+        Manager manager = service.updateManagerPassword(email, oldPassword, newPassword);
+        return manager;
+    }
+
+    @PostMapping(value = { "/managers/{email}", "/managers/{email}/" })
+    public Boolean updateManager(@PathVariable("email") String email){
+        Boolean manager = service.deleteManager(email);
+        return manager;
+    }
 }
