24be7b55aca173408e05e41572eb2e69d05523af
bmokhtari <bilar.mokhtari@mail.mcgill.ca>
2023-11-08 09:15:19 -0500

    Fixing Booking Controller

--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/controller/BookingRestController.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/controller/BookingRestController.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.Mar1HotelSystem.controller;
 
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,11 +13,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.Mar1HotelSystem.dto.BookingRequestDto;
+import ca.mcgill.ecse321.Mar1HotelSystem.dto.CustomerDto;
+import ca.mcgill.ecse321.Mar1HotelSystem.dto.RoomDto;
+import ca.mcgill.ecse321.Mar1HotelSystem.exception.Mar1HotelSystemException;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Booking;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Customer;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.GeneralUser;
+import ca.mcgill.ecse321.Mar1HotelSystem.model.Hotel;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Payment;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Room;
 import ca.mcgill.ecse321.Mar1HotelSystem.service.BookingService;
+import ca.mcgill.ecse321.Mar1HotelSystem.service.RoomService;
 
 /**
  * The controller that handles /booking endpoint requests
@@ -38,6 +45,12 @@ public class BookingRestController {
     @Autowired
     private BookingService bookingService;
 
+    @Autowired
+    private RoomService roomService;
+
+    @Autowired
+    private RoomDto roomRequestDto;
+
     @DeleteMapping(value = { "/booking/{bookingId}", "/booking/{bookingId}/" })
     @ResponseStatus(HttpStatus.OK)
     public void deleteBooking(@PathVariable int bookingId) {
@@ -56,21 +69,36 @@ public class BookingRestController {
         bookingService.getAllBookings();
     }
 
-//     @PostMapping
-//     @ResponseStatus(HttpStatus.CREATED)
-//     public void createBooking(@RequestBody BookingRequestDto bookingRequestDto) {
-//         Payment payment = bookingRequestDto.getPayment();
-//         GeneralUser generalUser = bookingRequestDto.getGeneralUser();
-//         Room room = bookingRequestDto.getRoom();
-//         bookingService.createBooking(payment, generalUser, room);
-//     }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createBooking(@RequestBody BookingRequestDto bookingRequestDto) {
+        GeneralUser generalUser = new GeneralUser(bookingRequestDto.getGeneralUser().getFirstName(), bookingRequestDto.getGeneralUser().getLastName(), bookingRequestDto.getGeneralUser().getEmail(), bookingRequestDto.getGeneralUser().getPhoneNumber());
+        Payment payment = new Payment(bookingRequestDto.getPayment().getAmount());
+        Room room = convertRoom(bookingRequestDto);
+        bookingService.createBooking(payment, generalUser, room);
+    }
 
-//     @PutMapping(value = { "/booking/{bookingId}", "/booking/{bookingId}/" })
-//     @ResponseStatus(HttpStatus.OK)
-//     public void updateBooking(@PathVariable int bookingId, @RequestBody BookingRequestDto bookingRequestDto) {
-//         Payment payment = bookingRequestDto.getPayment();
-//         GeneralUser generalUser = bookingRequestDto.getGeneralUser();
-//         Room room = bookingRequestDto.getRoom();
-//         bookingService.updateBooking(bookingId, payment, generalUser, room);
-//     }
+    @PutMapping(value = { "/booking/{bookingId}", "/booking/{bookingId}/" })
+    @ResponseStatus(HttpStatus.OK)
+    public void updateBooking(@PathVariable int bookingId, @RequestBody BookingRequestDto bookingRequestDto) {
+        GeneralUser generalUser = new GeneralUser(bookingRequestDto.getGeneralUser().getFirstName(), bookingRequestDto.getGeneralUser().getLastName(), bookingRequestDto.getGeneralUser().getEmail(), bookingRequestDto.getGeneralUser().getPhoneNumber());
+        Payment payment = new Payment(bookingRequestDto.getPayment().getAmount());
+        Booking booking = bookingService.getBookingById(bookingId);
+        booking.setGeneralUser(generalUser);
+        booking.setPayment(payment);
+        booking.setRoom(convertRoom(bookingRequestDto)); 
+        bookingService.updateBooking(booking);
+    }
+
+
+    private Room convertRoom (BookingRequestDto bookingRequestDto) {
+        Room.RoomType roomType = roomRequestDto.getRoomType();
+        Room.BedType bedType = roomRequestDto.getBedType();
+        boolean isAvailable = roomRequestDto.getIsAvailable();
+        int pricePerNight = roomRequestDto.getPricePerNight();
+        int maxCapacity = roomRequestDto.getMaxCapacity();
+
+        Room room = roomService.createRoom(roomType, bedType, isAvailable, pricePerNight, maxCapacity);
+        return room;
+    }
  }
