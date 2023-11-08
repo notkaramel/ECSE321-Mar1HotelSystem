d289b350074e7f0a5e2ecf50fe5831d20635e465
Antoine Phan <antoine.phan@mail.mcgill.ca>
2023-11-07 22:46:42 -0500

    Update the exception handler

--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/exception/Mar1HotelSystemExceptionHandler.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/exception/Mar1HotelSystemExceptionHandler.java
@@ -3,12 +3,13 @@ package ca.mcgill.ecse321.Mar1HotelSystem.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class Mar1HotelSystemExceptionHandler extends Throwable {
-        @ExceptionHandler(Mar1HotelSystemException.class)
-        public ResponseEntity<String> handleMar1HotelSystemException(Mar1HotelSystemException e) {
-            return new ResponseEntity<String>(e.getMessage(), e.getStatus());
+public class Mar1HotelSystemExceptionHandler extends ResponseEntityExceptionHandler {
 
-        }
+    @ExceptionHandler(Mar1HotelSystemException.class)
+    public ResponseEntity<String> handleMar1HotelSystemException(Mar1HotelSystemException e) {
+        return new ResponseEntity<String>(e.getMessage(), e.getStatus());
+    }
 }
