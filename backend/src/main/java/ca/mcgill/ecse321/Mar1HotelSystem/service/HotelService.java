9a5f22c1b90f5c042d52e8a0b0763313c9d95f9c
Antoine Phan <antoine.phan@mail.mcgill.ca>
2023-11-07 21:41:52 -0500

    Hotel Service and its tests are completed

--- backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/service/HotelService.java
+++ backend/src/main/java/ca/mcgill/ecse321/Mar1HotelSystem/service/HotelService.java
@@ -1,9 +1,11 @@
 package ca.mcgill.ecse321.Mar1HotelSystem.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import ca.mcgill.ecse321.Mar1HotelSystem.dao.HotelRepository;
+import ca.mcgill.ecse321.Mar1HotelSystem.exception.Mar1HotelSystemException;
 import ca.mcgill.ecse321.Mar1HotelSystem.model.Hotel;
 import jakarta.transaction.Transactional;
 
@@ -20,24 +22,23 @@ public class HotelService {
     HotelRepository hotelRepository;
 
     @Transactional
-    public boolean createHotel() {
-        try {
-            Hotel hotel = new Hotel();
-            hotelRepository.save(hotel);
-            return true;
-        } catch (Exception e) {
-            return false;
+    public Hotel createHotel() {
+        Hotel hotel = hotelRepository.findHotelByHotelName("Mar-1 Hotel");
+        if (hotel != null) {
+            throw new Mar1HotelSystemException(HttpStatus.BAD_REQUEST, "Can't create hotel");
         }
+        
+        hotel = new Hotel();
+        hotelRepository.save(hotel);
+        return hotel;
     }
 
     @Transactional
     public Hotel getHotel() {
-        return hotelRepository.findHotelByHotelName("Mar-1 Hotel");
-    }
-
-    @Transactional
-    public Hotel getHotel(String hotelName) {
-        // The hotel name should be "Mar-1 Hotel" by default
-        return hotelRepository.findHotelByHotelName(hotelName);
+        Hotel hotel = hotelRepository.findHotelByHotelName("Mar-1 Hotel");
+        if (hotel == null) {
+            throw new Mar1HotelSystemException(HttpStatus.BAD_REQUEST, "Can't find hotel");
+        }
+        return hotel;
     }
 }
--- backend/src/test/java/ca/mcgill/ecse321/Mar1HotelSystem/service/Mar1HotelSystemServiceTest.java
+++
@@ -1,5 +0,0 @@
-package ca.mcgill.ecse321.Mar1HotelSystem.service;
-
-public class Mar1HotelSystemServiceTest {
-    
-}
