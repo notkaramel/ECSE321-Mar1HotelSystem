77275107ead208c0a6e9901a68f691cd29d0c0e6
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2023-11-21 22:24:14 -0500

    adding a path for EmployeeView.vue in the router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RoomView from '../views/RoomView.vue'
+import EmployeeView from '../views/EmployeeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/rooms',
       name: 'rooms',
       component: RoomView
+    },
+    {
+      path: '/employees',
+      name: 'employees',
+      component: EmployeeView
     }
   ]
 })
