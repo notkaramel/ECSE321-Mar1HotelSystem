6dc1511fd53eff5c1e07334d189566dd7b7391d7
Antoine Phan <antoine.phan@mail.mcgill.ca>
2023-11-25 12:14:51 -0500

    Add route for viewing booking summary

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RoomView from '../views/RoomView.vue'
+import ViewBookingView from '../views/BookingSummaryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,8 +15,15 @@ const router = createRouter({
       path: '/rooms',
       name: 'rooms',
       component: RoomView
+    },
+    {
+      path: '/booking/summary/:bookingId',
+      name: 'Booking Summary View',
+      component: ViewBookingView,
+      props: route => ({ query: route.query.bookingId })
     }
-  ]
+  ],
+
 })
 
 export default router
