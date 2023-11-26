15773c49861817721eef4951521db8d315cd37a4
Antoine Phan <antoine.phan@mail.mcgill.ca>
2023-11-25 22:52:49 -0500

    User now able to cancel their booking

--- frontend/src/components/BookingSummary.vue
+++ frontend/src/components/BookingSummary.vue
@@ -1,50 +1,55 @@
 <template>
     <div class="card">
-        <div class="p-5 my-3 w-96">
-            <p class="text-2xl font-semibold">
-                Booking Summary
+        <p class="text-2xl font-semibold">
+            Booking Summary
+        </p>
+        <p class="text-xl text-green-600">
+            Booking ID: {{ bookingId }}
+        </p>
+        <div class="info">
+            <h1>
+                User Info:
+            </h1>
+            <p>
+                User Name: {{ user.firstName || "?" }} {{ user.lastName || "?" }} <br />
+                User email: {{ user.email || "?" }} <br />
+                User phone number: {{ user.phoneNumber || " " }}
             </p>
-            <p class="text-xl text-green-600">
-                Booking ID: {{ bookingId }}
+        </div>
+        <div class="info">
+            <h1>
+                Room booked:
+            </h1>
+            <p>
+                Room ID: {{ room.roomId || "?" }} <br />
+                Room Price Per Night: {{ room.pricePerNight || "?" }}
             </p>
-            <div class="info">
-                <h1>
-                    User Info:
-                </h1>
-                <p>
-                    User Name: {{ user.firstName || " " }} {{ user.lastName || " " }} <br />
-                    User email: {{ user.email || " " }} <br />
-                    User phone number: {{ user.phoneNumber || " " }}
-                </p>
-            </div>
-            <div class="info">
-                <h1>
-                    Room booked:
-                </h1>
-                <p>
-                    Room ID: {{ room.roomId || " " }} <br />
-                    Room Price Per Night: {{ room.pricePerNight || " " }}
-                </p>
-                <p>
-                    Room Type: {{ room.roomType || " " }}
-                </p>
-            </div>
-            <div class="info">
-                <h1>Payment Info</h1>
-                <p>
-                    Amount: CA${{ payment.amount || " " }} <br />
-                    Payment ID: {{ payment.paymentId || " " }}
-                </p>
-            </div>
+            <p>
+                Room Type: {{ room.roomType || "?" }}
+            </p>
+        </div>
+        <div class="info">
+            <h1>Payment Info</h1>
+            <p>
+                Amount: CA${{ payment.amount || "?" }} <br />
+                Payment ID: {{ payment.paymentId || "?" }}
+            </p>
+        </div>
+        <div id="cancel-booking">
+            <p> Booked by mistake? Cancel now with full refund! </p>
+            <button class="w-full bg-rose-600 text-white font-semibold py-3" @click="cancelBooking">
+                Cancel
+            </button>
         </div>
     </div>
 </template>
 
 <style scoped lang="postcss">
 .card {
-    @apply block w-1/4 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100;
+    @apply block w-1/2 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100;
     @apply dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700;
 }
+
 .BookingSummaryCard {
     @apply m-12;
 }
@@ -64,6 +69,7 @@
 
 <script lang="ts">
 import { useRoute } from 'vue-router';
+import axios from 'axios';
 
 export default {
     name: 'BookingSummaryView',
@@ -71,42 +77,41 @@ export default {
         bookingId: Number,
     },
     methods: {
-        async setup() {
+        async loadSummary() {
             const backend = import.meta.env.VITE_BACKEND;
             const route = useRoute();
             const bookingId = route.params.bookingId;
-            console.log(bookingId);
-
-            console.log(backend + '/booking/' + bookingId);
 
             let bookingInfo = await fetch(backend + '/booking/' + bookingId)
                 .then(response => {
-                    // console.log(response);
                     return response.json();
                 })
                 .then(json => {
-                    // console.log(json);
-                    // console.log(json["bookingId"]);
                     const user = json["generalUser"];
                     const room = json["room"];
                     const payment = json["payment"];
-                    console.log(user);
-                    console.log(room);
-                    console.log(payment);
                     this.user = user;
                     this.room = room;
                     this.payment = payment;
                     return json;
-
                 })
                 .catch(error => {
                     console.log(error);
                 });
-
-            console.log(bookingInfo);
             return {
                 bookingInfo
             }
+        },
+        async cancelBooking() {
+            const backend = import.meta.env.VITE_BACKEND;
+            const bookingId = this.bookingId;
+            await axios.delete(backend + '/booking/delete/' + bookingId)
+                .then(response => {
+                    return (response.status == 200 ? alert("Booking cancelled successfully!") : null);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     },
     data() {
@@ -136,7 +141,7 @@ export default {
         }
     },
     beforeMount() {
-        this.setup();
+        this.loadSummary();
     }
 }
 </script>
